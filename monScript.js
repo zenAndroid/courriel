28c569a187349b86a986be2f799b762cfab59576
zenAndroid <zena02@uqo.ca>
2018-02-26 16:24:38 -0500

    Initial commit

---
+++ monScript.js
@@ -0,0 +1,58 @@
+function afficherAcceuil(){
+    document.getElementById('acceuil').style.display = "block";
+    document.getElementById('nouveau').style.display = "none";
+    document.getElementById('messages').style.display = "none";
+    document.getElementById('contacts').style.display = "none";
+}
+
+function afficherMessages(){
+    document.getElementById('acceuil').style.display = "none";
+    document.getElementById('nouveau').style.display = "none";
+    document.getElementById('messages').style.display = "block";
+    document.getElementById('contacts').style.display = "none";
+}
+
+function afficherNouveau(){
+    document.getElementById('acceuil').style.display = "none";
+    document.getElementById('nouveau').style.display = "block";
+    document.getElementById('messages').style.display = "none";
+    document.getElementById('contacts').style.display = "none";
+}
+
+function afficherAdresses(){
+    document.getElementById('acceuil').style.display = "none";
+    document.getElementById('nouveau').style.display = "none";
+    document.getElementById('messages').style.display = "none";
+    document.getElementById('contacts').style.display = "block";
+}
+
+function ajouterContact(){
+    var li = document.createElement("li");
+    var nomContact = document.getElementById("pseudo").value;
+    if (nomContact != "") {
+        li.innerText = nomContact;
+        var ul = document.getElementById("listeContact");
+        ul.appendChild(li);
+        document.getElementById("pseudo").value="";
+    }
+    else{
+        alert("Veuillez donnez le nom du contact");
+    }
+}
+ function toto() {
+     var destinataire = document.getElementById("dest").value;
+     var message = document.getElementById("body").value;
+     var Message = "";
+     if (destinataire != "") {
+         var MsgHeader = "Destinataire : " + destinataire;
+         if (message != "") {
+             var MsgBody = "Message : " + message;
+             Message = MsgHeader + '\n' + MsgBody;
+             alert(Message);
+         } else {
+             alert("Veuillez remplir le message");
+         }
+     } else {
+         alert("Veuillez entrez le nom du destinataire !");
+     }
+ }
