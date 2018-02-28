89d0d3f082deb71f29ce30769aeff5e3d0b41e6a
zenAndroid <zena02@uqo.ca>
2018-02-28 00:08:37 -0500

    Add ajouterMessage(), add font, fix some css

--- monScript.js
+++ monScript.js
@@ -1,27 +1,27 @@
 function afficherAcceuil(){
-    document.getElementById('acceuil').style.display = "block";
-    document.getElementById('nouveau').style.display = "none";
+    document.getElementById('acceuil').style.display  = "block";
+    document.getElementById('nouveau').style.display  = "none";
     document.getElementById('messages').style.display = "none";
     document.getElementById('contacts').style.display = "none";
 }
 
 function afficherMessages(){
-    document.getElementById('acceuil').style.display = "none";
-    document.getElementById('nouveau').style.display = "none";
+    document.getElementById('acceuil').style.display  = "none";
+    document.getElementById('nouveau').style.display  = "none";
     document.getElementById('messages').style.display = "block";
     document.getElementById('contacts').style.display = "none";
 }
 
 function afficherNouveau(){
-    document.getElementById('acceuil').style.display = "none";
-    document.getElementById('nouveau').style.display = "block";
+    document.getElementById('acceuil').style.display  = "none";
+    document.getElementById('nouveau').style.display  = "block";
     document.getElementById('messages').style.display = "none";
     document.getElementById('contacts').style.display = "none";
 }
 
 function afficherAdresses(){
-    document.getElementById('acceuil').style.display = "none";
-    document.getElementById('nouveau').style.display = "none";
+    document.getElementById('acceuil').style.display  = "none";
+    document.getElementById('nouveau').style.display  = "none";
     document.getElementById('messages').style.display = "none";
     document.getElementById('contacts').style.display = "block";
 }
@@ -39,7 +39,8 @@ function ajouterContact(){
         alert("Veuillez donnez le nom du contact");
     }
 }
- function toto() {
+
+function toto() {
      var destinataire = document.getElementById("dest").value;
      var message = document.getElementById("body").value;
      var Message = "";
@@ -56,3 +57,14 @@ function ajouterContact(){
          alert("Veuillez entrez le nom du destinataire !");
      }
  }
+
+function ajouterMessage(){
+    var table              = document.getElementById("tableDeMessages");
+    var row                = table.insertRow(1);
+
+    var auteur             = row.insertCell(0);
+    var message            = row.insertCell(1);
+
+    auteur.innerHTML       = "FOO";
+    message.innerHTML      = "BAR";
+}
